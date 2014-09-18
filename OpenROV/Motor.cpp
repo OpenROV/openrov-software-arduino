1b28885defdb0543db897345a9a3b5ceb6ae2c07
BrianAdams <brian@babastik.com>
2014-09-17 20:25:36 -0700

    issue openrov-software#288; added deadzone skip

--- OpenROV/Motor.cpp
+++ OpenROV/Motor.cpp
@@ -10,6 +10,8 @@ void Motor::attachPin(int motor_pin){
   _motor_pin = motor_pin;
   motor_positive_modifer = 1.0;
   motor_negative_modifer = 2.0;
+  motor_deadzone_negative = 25;
+  motor_deadzone_positive = 25;
 }
 
 void Motor::reset(){
@@ -21,7 +23,16 @@ int Motor::goms(int ms){
   if (ms>MIDPOINT) modifier = motor_positive_modifer;
   if (ms<MIDPOINT) modifier = motor_negative_modifer;
   int delta = ms-MIDPOINT;
-  _servo.writeMicroseconds(constrain(MIDPOINT+delta*modifier,1000,2000));
+  //map around deadzone
+  int predeadzonems = constrain(MIDPOINT+delta*modifier,1000,2000);
+  int finalms = 1500;
+  if (predeadzonems < 0){
+     finalms = map(predeadzonems, 1000, 1500, 1000, 1500-motor_deadzone_negative);
+  } else if (predeadzonems > 0) {
+     finalms = map(predeadzonems, 1500, 2000, 1500+motor_deadzone_positive, 2000);
+  }
+
+  _servo.writeMicroseconds(constrain(finalms,1000,2000));
   return _servo.readMicroseconds();
 }
 
