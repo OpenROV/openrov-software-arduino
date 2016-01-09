3f6a0d17edce12abcd58b2b0791474cd1f03a48d
Charles Cross <charles@openrov.com>
2016-01-09 02:42:11 +0000

    Working Serial0 and Serial1 example

--- sketches/OpenROV/OpenROV.ino
+++ sketches/OpenROV/OpenROV.ino
@@ -5,8 +5,8 @@ void setup()
 	//digitalWrite(PIN_LED_0, HIGH);
 	//digitalWrite(PIN_LED_1, HIGH);
 
-	//Serial.begin( 115200 );
-	//Serial1.begin( 115200 );
+	Serial.begin( 115200 );
+	Serial1.begin( 115200 );
 }
 
 void loop()
@@ -16,6 +16,6 @@ void loop()
   	digitalWrite(PIN_LED, LOW);    // turn the LED off by making the voltage LOW
  	delay(1000);
 
-	//Serial.println( "Hello0" );
-	//Serial1.println( "Hello1" );   
+	Serial.println( "Hello0" );
+	Serial1.println( "Hello1" );   
 }
