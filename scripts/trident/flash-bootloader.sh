7e335ce313b81676231cb09319f169310994325a
Charles Cross <charles@openrov.com>
2016-07-07 12:25:46 -0700

    Deleted unused libraries

--- scripts/trident/flash-bootloader.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-BOOTLOADER_PATH=/opt/openrov/arduino/hardware/openrov/samd/bootloaders/samd21
-
-openocd -f ${BOOTLOADER_PATH}/openocd.cfg -c "program ${BOOTLOADER_PATH}/samba_both.hex; reset; exit"
--- scripts/trident/rpi/flash-bootloader.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PROJECT_DIR=/opt/openrov/firmware
-BIN_DIR=${PROJECT_DIR}/bin/trident
-
-BOOTLOADER_PATH=/opt/openrov/arduino/hardware/openrov/samd/bootloaders/samd21
-
-openocd -f ${BOOTLOADER_PATH}/openocd.cfg -c "program ${BIN_DIR}/OpenROV.bin; reset; exit"
