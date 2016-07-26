91b8cdba6f1ca5fb56946475b1fd7b80d2c9caf7
Charles Cross <charles@openrov.com>
2016-07-26 16:44:34 -0700

    Added sysconfig file for transitive orov2x sketch

---
+++ sketches/OpenROV2x/SysConfig.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "SystemConstants.h"
+#include "CompileOptions.h"
+
+// TODO: The MCU ARCH and Board Type should eventually be passed in solely from the build script.
+
+// ---------------------------------------------------------
+// MCU Architecture Selection
+// ---------------------------------------------------------
+#define MCUARCH			0
+#define MCUARCH_AVR 	1
+#define MCUARCH_SAMD 	2
+
+// Select here
+#define MCUARCH			MCUARCH_AVR
+
+#if MCUARCH == 0
+	#error "SysConfig.h: You must select an MCU architecture!"
+#endif
+
+// ---------------------------------------------------------
+// Controller Board Selection
+// ---------------------------------------------------------
+#define CONTROLLERBOARD					0
+#define CONTROLLERBOARD_CAPE 			1
+#define CONTROLLERBOARD_CB25			2
+#define CONTROLLERBOARD_TRIDENT			3
+
+// Select here
+#define CONTROLLERBOARD					CONTROLLERBOARD_CB25
+
+#if CONTROLLERBOARD == 0
+	#error "SysConfig.h: You must select a board type!"
+#endif
