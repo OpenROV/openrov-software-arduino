c486dca97a202d66e223ac02a74b9b20861dd77d
Charles Cross <charles@openrov.com>
2016-08-08 16:39:53 -0700

    Created float input version of mapf

--- sketches/Trident/Utility.cpp
+++ sketches/Trident/Utility.cpp
@@ -22,4 +22,9 @@ namespace util
 	{
 		return ( float )( x - in_min ) * ( out_max - out_min ) / ( float )( in_max - in_min ) + out_min;
 	}
+
+	float mapf( float x, float in_min, float in_max, float out_min, float out_max )
+	{
+		return ( x - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
+	}
 }
