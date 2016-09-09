cc3107f4351e37967e8ef4e4887d66b93a9eb4a5
Gilbert Montague <gilbert.i.montague@gmail.com>
2016-09-08 17:31:59 -0700

    Semicolon

--- libraries/MPL3115A2/MPL3115A2.h
+++ libraries/MPL3115A2/MPL3115A2.h
@@ -55,7 +55,7 @@ namespace mpl3115a2
 
         public:
 
-            MPL3115A2( CI2C *i2cInterfaceIn, int32_t sensorIdIn = -1, uint8_t addressIn = MPL3115A2_ADDRESS )
+            MPL3115A2( CI2C *i2cInterfaceIn, int32_t sensorIdIn = -1, uint8_t addressIn = MPL3115A2_ADDRESS );
 
             //Public member functions
             ERetCode ReadPressure( float& pressureOut );
