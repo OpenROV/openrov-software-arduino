74600fb024e4428b7275224eaf47eee01f4b5e22
Gilbert Montague <gilbert.i.montague@gmail.com>
2016-09-09 18:12:25 -0700

    Removed redundant reads

--- libraries/MPL3115A2/MPL3115A2.cpp
+++ libraries/MPL3115A2/MPL3115A2.cpp
@@ -130,19 +130,20 @@ ERetCode MPL3115A2::ReadPressure( float& pressureOut )
 
     int32_t returnCode;
     uint8_t pdr;
-    
+
     //Wait for PDR bit, indicates we have new pressure data
     auto counter = 0;
     while( ( pdr & (1<<2) ) == 0 )
     {
+        Serial.println( counter );
         returnCode = ReadByte( MPL3115A2_REGISTER::STATUS, pdr );
         if( returnCode != I2C::ERetCode::SUCCESS )
         {
             return ERetCode::FAILED_PRESSURE_READ;
         }
 
-        {
         if( ++counter > 100 )
+        {
             return ERetCode::TIMED_OUT;
         }
         delay(1);
