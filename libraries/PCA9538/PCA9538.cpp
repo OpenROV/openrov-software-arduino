7537e5777063a3bb76fa5318be2d3e85df90d33a
Gilbert Montague <gilbert.i.montague@gmail.com>
2016-09-14 15:15:39 -0700

    pca9538 init commit

---
+++ libraries/PCA9538/PCA9538.cpp
@@ -0,0 +1,42 @@
+/* 
+ PCA9539 GPIO Expander Library
+ By: OpenROV
+ Date: September 14, 2016
+*/
+
+#include <Arduino.h>
+#include <CI2C.h>
+
+#include "PCA9539.h"
+
+using namespace pca9539;
+
+PCA9539::PCA9539( CI2C* i2cInterfaceIn )
+    : m_i2cAddress( pca9539::PCA9539_ADDRESS )
+    , m_pI2C( i2cInterfaceIn )
+{
+    
+}
+
+
+
+
+
+
+/***************************************************************************
+    PRIVATE FUNCTIONS
+ ***************************************************************************/
+int32_t PCA9539::WriteByte( uint8_t addressIn, uint8_t dataIn )
+{
+	return (int32_t)m_pI2C->WriteByte( m_i2cAddress, (uint8_t)addressIn, dataIn );
+}
+
+int32_t PCA9539::ReadByte( uint8_t addressIn, uint8_t &dataOut )
+{
+	return (int32_t)m_pI2C->ReadByte( m_i2cAddress, (uint8_t)addressIn, &dataOut );
+}
+int32_t PCA9539::ReadNBytes( uint8_t addressIn, uint8_t* dataOut, uint8_t byteCountIn )
+{
+    return (int32_t)m_pI2C->ReadBytes( m_i2cAddress, (uint8_t)addressIn, dataOut, byteCountIn );
+}
+
---
+++ libraries/PCA9538/PCA9539.h
@@ -0,0 +1,50 @@
+/* 
+ PCA9539 GPIO Expander Library
+ By: OpenROV
+ Date: September 14, 2016
+*/
+
+#pragma once
+
+//Forward declaration of the I2C interface
+class CI2C;
+
+namespace pca9539
+{
+    //Unshifted 7-bit I2C address for device
+    const uint8_t PCA9539_ADDRESS = 0x70; //b1110000
+
+    enum ERetCode : int32_t
+    {
+        SUCCESS,
+        FAILED,
+        TIMED_OUT,
+        UNKNOWN
+    };
+
+    class PCA9539
+    {
+        public:
+            PCA9539( CI2C* i2cInterfaceIn );
+
+            ERetCode Initialize();
+            bool IsInitialized() const { return m_isInitialized; };
+
+        private:
+            //Private member functions
+
+
+            
+            int32_t ReadByte( MPL3115A2_REGISTER addressIn, uint8_t& dataOut );
+            int32_t ReadNBytes( MPL3115A2_REGISTER addressIn, uint8_t* dataOut, uint8_t byteCountIn );
+            int32_t WriteByte( MPL3115A2_REGISTER addressIn, uint8_t dataIn );
+
+
+            //Private member attributes
+            uint8_t m_i2cAddress;
+            CI2C* m_pI2C;
+
+            bool m_isInitialized = false;
+
+    }
+}
