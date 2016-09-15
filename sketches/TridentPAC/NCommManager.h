7b4dbb685064bca66178a8ac30cbdeea737a9435
Charles Cross <charles@openrov.com>
2016-09-15 12:33:21 -0700

    Added todo notes in comm manager

--- sketches/TridentPAC/NCommManager.h
+++ sketches/TridentPAC/NCommManager.h
@@ -5,6 +5,13 @@
 
 namespace NCommManager
 {
+	enum class ERetCode
+	{
+		SUCCESS,				// Message successfully inserted in outbound buffer
+		OVERSIZED_MESSAGE,		// Message too large to send. Consider shortening or breaking into multiple messages
+		TX_BUFFER_FULL			// Currently no space in outbound buffer
+	};
+
 	// Variables
 	extern CCommand m_currentCommand;
 
@@ -13,4 +20,8 @@ namespace NCommManager
 	// Methods
 	extern void Initialize();
 	extern void GetCurrentCommand();
+
+	// TODO: Some kind of magic template function that lets Module devs easily send data across the UART
+	// template<class... TArgs>
+	// ERetCode SendMessage( uint32_t uuidIn, const char* behaviorIn, TArgs... args);
 }
