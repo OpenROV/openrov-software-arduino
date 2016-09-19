d3e2e43327bbad782310581bd343fb2fb075afc6
Charles Cross <charles@openrov.com>
2016-09-19 08:59:13 -0700

    Removed old versioning system, added command to request output of new autogenerated SHA1

--- sketches/OpenROV2x/NConfigManager.cpp
+++ sketches/OpenROV2x/NConfigManager.cpp
@@ -1,5 +1,6 @@
 #include "NConfigManager.h"
 #include "NArduinoManager.h"
+#include "CompileOptions.h"
 
 namespace NConfigManager
 {
@@ -16,36 +17,42 @@ namespace NConfigManager
 
 	void HandleMessages( CCommand& commandIn )
 	{
-		if( commandIn.Equals( "reportSetting" ) )
+		if( NCommManager::m_isCommandAvailable )
 		{
-			Serial.print( F( "*settings:" ) );
-			Serial.print( F( "smoothingIncriment|" ) );
-			Serial.print( String( NConfigManager::m_throttleSmoothingIncrement ) + "," );
-			Serial.print( F( "deadZone_min|" ) );
-			Serial.print( String( NConfigManager::m_deadZoneMin ) + "," );
-			Serial.print( F( "deadZone_max|" ) );
-			Serial.print( String( NConfigManager::m_deadZoneMax ) + "," );
-			Serial.print( F( "water_type|" ) );
-			Serial.println( String( NConfigManager::m_waterType ) + ";" );
+			if( commandIn.Equals( "version" ) )
+			{
+				// SHA1 hash of all of the source used to build the firmware
+				// This gets automatically generated at compile time by the ArduinoBuilder library and appended to CompileOptions.h
+				Serial.println( VERSION_HASH );
+			}
+			else if( commandIn.Equals( "reportSetting" ) )
+			{
+				Serial.print( F( "*settings:" ) );
+				Serial.print( F( "smoothingIncriment|" ) );
+				Serial.print( String( NConfigManager::m_throttleSmoothingIncrement ) + "," );
+				Serial.print( F( "deadZone_min|" ) );
+				Serial.print( String( NConfigManager::m_deadZoneMin ) + "," );
+				Serial.print( F( "deadZone_max|" ) );
+				Serial.print( String( NConfigManager::m_deadZoneMax ) + "," );
+				Serial.print( F( "water_type|" ) );
+				Serial.println( String( NConfigManager::m_waterType ) + ";" );
 
-		}
-		else if( commandIn.Equals( "rcap" ) ) //report capabilities
-		{
-			Serial.print( F( "CAPA:" ) );
-			Serial.print( m_capabilityBitmask );
-			Serial.print( ';' );
-
-			// TODO: Reimplement correctly
-			// NArduinoManager::ScanI2C();
-		}
-		else if( commandIn.Equals( "updateSetting" ) )
-		{
-			//TODO: Need to update the motors with new deadZone setting. Probably move
-			//deadzone to the thruster resposibilitiy
-			NConfigManager::m_throttleSmoothingIncrement = commandIn.m_arguments[1];
-			NConfigManager::m_deadZoneMin					= commandIn.m_arguments[2];
-			NConfigManager::m_deadZoneMax		= commandIn.m_arguments[3];
-			NConfigManager::m_waterType		= commandIn.m_arguments[4];
+			}
+			else if( commandIn.Equals( "rcap" ) ) //report capabilities
+			{
+				Serial.print( F( "CAPA:" ) );
+				Serial.print( m_capabilityBitmask );
+				Serial.print( ';' );
+			}
+			else if( commandIn.Equals( "updateSetting" ) )
+			{
+				//TODO: Need to update the motors with new deadZone setting. Probably move
+				//deadzone to the thruster resposibilitiy
+				NConfigManager::m_throttleSmoothingIncrement = commandIn.m_arguments[1];
+				NConfigManager::m_deadZoneMin					= commandIn.m_arguments[2];
+				NConfigManager::m_deadZoneMax		= commandIn.m_arguments[3];
+				NConfigManager::m_waterType		= commandIn.m_arguments[4];
+			}
 		}
 	}
 }
